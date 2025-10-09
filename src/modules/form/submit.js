a7ac4996175da9aac36ba2313b6bf2a4e5d5fc13
Evelynn <evelynnbr24@gmail.com>
2025-10-08 22:17:09 -0300

    pegando as info de inputs

---
+++ src/modules/form/submit.js
@@ -0,0 +1,39 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+const clientName = document.getElementById("client")
+const selectedDate = document.getElementById("date")
+
+// data atual para input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+// carrega a dat6a atual
+selectedDate.value = inputToday
+//define a data mínima como atual
+selectedDate.min = inputToday
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  try {
+    const name = clientName.value.trim()
+
+    if (!name) {
+      return alert("Informe o nome do cliente")
+    }
+
+    const hourSelected = document.querySelector(".hour-selected")
+
+    if (!hourSelected) {
+      return alert("Selecione um Horário")
+    }
+
+    const [hour] = hourSelected.innerText.split(":")
+
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+
+    const id = new Date().getTime()
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento")
+    console.log(error)
+  }
+}
